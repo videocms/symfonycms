addcedeec1bd7fdce87af7fcb9dbdfb1604e88c9
Mykyy <Mykyy@mykyy-pc>
2015-01-08 16:33:16 +0100

    start nowy projekt

---
+++ src/Cms/CmstmBundle/CmsCmstmBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Cms\CmstmBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CmsCmstmBundle extends Bundle
+{
+}
---
+++ src/Cms/CmstmBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cms\CmstmBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('CmsCmstmBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
